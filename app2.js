3a9dd9c873c579e5f521b1bb839a05d17f63e14d
solstxce <48374053+solstxce@users.noreply.github.com>
2024-09-17 02:00:01 +0530

    fix code

--- app2.js
+++ app2.js
@@ -692,7 +692,7 @@ app.get('/admin/low-stock-alerts', verifyToken, isAdmin, async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
